6f16e7635c903575b92d145d63c5dbb27a9ff73b
Nevillested <olegsidnev@gmail.com>
2022-05-14 00:12:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(timer));
+
+                myThread.Start();
+        }
+        public static void timer()
+        {
+            System.DateTime date1 = new DateTime(2022, 2, 16, 20, 0, 0);
+            while (true)
+            {
+                System.DateTime date2 = DateTime.Now;
+                Console.WriteLine("Текущее дата-время: " + date2 + "\r\n");
+                Console.WriteLine("Обнова будет: " + date1 + "\r\n");
+                System.TimeSpan diff1 = date1.Subtract(date2);
+                Console.WriteLine("Осталось ждать: " + diff1 + "\r\n");
+                Thread.Sleep(950);
+                Console.Clear();
+            }
+        }
+    }
+}
